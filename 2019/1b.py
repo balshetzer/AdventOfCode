2288f43c904af7444a2c0630356babb2f7df3dcf
Hesky Fisher <hesky@hesky-macbookpro.roam.corp.google.com>
2019-12-04 20:47:16 -0500

    day 1

---
+++ 2019/1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import fileinput
+
+def fuel(x):
+    f = x//3-2
+    return f + fuel(f) if f > 0 else 0
+        
+print(sum(fuel(int(line.strip())) for line in fileinput.input()))
