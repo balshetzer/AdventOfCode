1679431368e035fddf5d7e466b15ac8800b442f9
Hesky Fisher <hesky.fisher@gmail.com>
2020-12-05 09:02:35 -0500

    make day 5 more clever

--- 2020/5a.py
+++ 2020/5a.py
@@ -2,20 +2,6 @@
 
 import fileinput
 
-def seat(s):
-  rowend = 128
-  rowbegin = 0
-  colend = 8
-  colbegin = 0
-  for c in s:
-    if c == "F":
-      rowend -= (rowend-rowbegin)//2
-    elif c == "B":
-      rowbegin += (rowend-rowbegin)//2
-    elif c == "L":
-      colend -= (colend-colbegin)//2
-    elif c == "R":
-      colbegin += (colend-colbegin)//2
-  return 8 * rowbegin + colbegin
-
+trans = str.maketrans('BFRL', '1010')
+seat = lambda s: int(s.translate(trans), 2)
 print(max(seat(line) for line in fileinput.input()))
--- 2020/5b.py
+++ 2020/5b.py
@@ -2,25 +2,9 @@
 
 import fileinput
 
-def seat(s):
-  rowend = 128
-  rowbegin = 0
-  colend = 8
-  colbegin = 0
-  for c in s:
-    if c == "F":
-      rowend -= (rowend-rowbegin)//2
-    elif c == "B":
-      rowbegin += (rowend-rowbegin)//2
-    elif c == "L":
-      colend -= (colend-colbegin)//2
-    elif c == "R":
-      colbegin += (colend-colbegin)//2
-  return 8 * rowbegin + colbegin
-
-seats = {seat(line) for line in fileinput.input()}
-
-for i in range(max(seats)):
-  if i not in seats and i-1 in seats and i+1 in seats:
-    print(i)
-    break
+trans = str.maketrans('BFRL', '1010')
+seat = lambda s: int(s.translate(trans), 2)
+passes = {seat(line) for line in fileinput.input()}
+seats = set(range(min(passes), max(passes)+1))
+mine, = seats - passes
+print(mine)
