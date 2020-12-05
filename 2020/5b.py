7250e027e95a8855c7e88643ff0396a0873a17cd
Hesky Fisher <hesky.fisher@gmail.com>
2020-12-05 07:32:47 -0500

    day 5 initial solution

---
+++ 2020/5b.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+def seat(s):
+  rowend = 128
+  rowbegin = 0
+  colend = 8
+  colbegin = 0
+  for c in s:
+    if c == "F":
+      rowend -= (rowend-rowbegin)//2
+    elif c == "B":
+      rowbegin += (rowend-rowbegin)//2
+    elif c == "L":
+      colend -= (colend-colbegin)//2
+    elif c == "R":
+      colbegin += (colend-colbegin)//2
+  return 8 * rowbegin + colbegin
+
+seats = {seat(line) for line in fileinput.input()}
+
+for i in range(max(seats)):
+  if i not in seats and i-1 in seats and i+1 in seats:
+    print(i)
+    break
