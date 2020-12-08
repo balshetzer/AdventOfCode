be15c1774e0b33e5cba29d08213b973082a9f163
Hesky Fisher <hesky.fisher@gmail.com>
2020-12-07 23:15:00 -0500

    improve day 7

--- 2020/7a.py
+++ 2020/7a.py
@@ -2,26 +2,19 @@
 
 import sys
 import re
+from collections import defaultdict
 
 input = open(sys.argv[1]) if len(sys.argv) == 2 else sys.stdin
 
-# rules = {}
-# for line in input.read().splitlines()):
-#   left, right = line.split(' bags contain ')
-#   if right == 'no other bags':
-#     right = set()
-#   else:
-#     right = {x.split(' ', 1)[1] for x in right.split(', ')}
-#   rules[left] = right
+rules = defaultdict(set)
+for line in input.read().splitlines():
+  left = line.split(' bags', 1)[0]
+  for bag in re.findall(r'\d+ ([^,]+) bags?[,.]', line):
+    rules[bag].add(left)
 
-rules = dict(line.split(' bags contain ') for line in input.read().splitlines())
-
-wanted = {'shiny gold'}
-containers = set()
-
-while wanted:
-  wanted = {key for key, value in rules.items() if any(re.search(r'\d ' + bag + ' bag', value) for bag in wanted)}
-  containers |= wanted
-  
-print(len(containers))
-
+bags = set()
+q = {'shiny gold'}
+while q:
+  q = {b for x in q for b in rules[x]}
+  bags |= q
+print(len(bags))
--- 2020/7b.py
+++ 2020/7b.py
@@ -2,32 +2,11 @@
 
 import sys
 import re
-import functools
 
 input = open(sys.argv[1]) if len(sys.argv) == 2 else sys.stdin
 
-# rules = {}
-# for line in input.read().splitlines()):
-#   left, right = line.split(' bags contain ')
-#   if right == 'no other bags':
-#     right = set()
-#   else:
-#     right = {x.split(' ', 1)[1] for x in right.split(', ')}
-#   rules[left] = right
-
 rules = dict(line.split(' bags contain ') for line in input.read().splitlines())
 
-@functools.lru_cache(maxsize=None)
-def contains(bag):
-  v = rules[bag]
-  if v == 'no other bags.':
-    return 0
-  total = 0
-  for entry in v.split(', '):
-    n, rest = entry.split(' ', 1)
-    n = int(n)
-    bag, _ = rest.rsplit(' ', 1)
-    total += n + n * contains(bag)
-  return total
+contents = lambda bag: sum(int(n) * (1 + contents(b)) for n, b in re.findall(r'(\d+) ([^,]+) bags?[,.]', rules[bag]))
 
-print(contains('shiny gold'))
+print(contents('shiny gold'))
