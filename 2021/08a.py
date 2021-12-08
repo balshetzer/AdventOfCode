9329bbb758d61772b31db286e6c70bb50cb31988
Hesky Fisher <hesky.fisher@gmail.com>
2021-12-08 18:45:37 -0500

    day 8 part 1

---
+++ 2021/08a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+lines = (map(len, line.split('|')[1].split()) for line in fileinput.input())
+print(sum(sum(1 for n in line if n in (2, 4, 3, 7)) for line in lines))
