4640fcd4db87374626d15346001bfa09c6e842d7
Hesky Fisher <hesky.fisher@gmail.com>
2021-12-10 18:50:39 -0500

    reduce the deductions to what's needed in b change tuple to set in a

--- 2021/08a.py
+++ 2021/08a.py
@@ -3,4 +3,4 @@
 import fileinput
 
 lines = (map(len, line.split('|')[1].split()) for line in fileinput.input())
-print(sum(sum(1 for n in line if n in (2, 4, 3, 7)) for line in lines))
+print(sum(sum(1 for n in line if n in {2, 4, 3, 7}) for line in lines))
