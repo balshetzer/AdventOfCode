b4c0c659a6ee9323fd9869bc49e1314b7a970b23
Hesky Fisher <hesky.fisher@gmail.com>
2021-12-11 11:43:20 -0500

    day 10 part b

---
+++ 2021/10b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import fileinput
+from itertools import pairwise
+from statistics import median
+from functools import reduce
+from more_itertools import chunked
+
+lines = (line.strip() for line in fileinput.input())
+ends = {closer: opener for opener, closer in chunked('()[]{}<>', 2, True)}
+bounty = {'(': 1, '[': 2, '{': 3, '<': 4}
+
+def score(line):
+  stack = []
+  for c in line:
+    if c in ends:
+      opener = stack.pop()
+      if ends[c] != opener:
+        return None
+    else:
+      stack.append(c)
+  return reduce(lambda score, opener: score*5 + bounty[opener], reversed(stack), 0)
+
+print(median(filter(lambda x: x is not None, map(score, lines)))) 
