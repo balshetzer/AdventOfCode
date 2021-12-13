77dc1e82b86b4b290247bbefe97932cc954ad7c7
Hesky Fisher <hesky.fisher@gmail.com>
2021-12-12 23:32:55 -0500

    a little faster

--- 2021/12b.py
+++ 2021/12b.py
@@ -3,31 +3,29 @@
 import fileinput
 from collections import defaultdict
 
-graph = defaultdict(list)
-for left, right in (line.strip().split('-') for line in fileinput.input()):
-  graph[left].append(right)
-  graph[right].append(left)
+def run():
+  graph = defaultdict(list)
+  for left, right in (line.strip().split('-') for line in fileinput.input()):
+    graph[left].append(right)
+    graph[right].append(left)
   
-paths = [('start', set(), False)]
+  paths = [('start', set(), False)]
+  finished = 0
 
-finished = 0
+  while paths:
+    path = paths.pop()
+    tip, visited, doubled = path
+    for n in graph[tip]:
+      if n == 'start':
+        pass
+      elif n == 'end':
+        finished += 1
+      elif n[0].isupper():
+        paths.append((n, visited, doubled))
+      elif not n in visited:
+        paths.append((n, visited | {n}, doubled))
+      elif not doubled:
+        paths.append((n, visited, True))
+  print(finished)
 
-def expand(path):
-  global finished, paths
-  tip, visited, doubled = path
-  for n in graph[tip]:
-    if n == 'start':
-      pass
-    elif n == 'end':
-      finished += 1
-    elif n.isupper():
-      paths.append((n, visited, doubled))
-    elif not n in visited:
-      paths.append((n, visited | {n}, doubled))
-    elif not doubled:
-      paths.append((n, visited, True))
-
-while paths:
-  expand(paths.pop())
-
-print(finished)
+run()
