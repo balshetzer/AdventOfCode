fcbfeb99f409e5fd0e0e5ca95d3653ea0ceabfd8
Hesky Fisher <hesky.fisher@gmail.com>
2022-12-01 22:08:05 -0500

    simplify

--- 2022/01b.py
+++ 2022/01b.py
@@ -4,6 +4,5 @@ from sys import argv, stdin
 
 f = open(argv[1]) if len(argv) == 2 else stdin
 
-calories = [sum(int(line) for line in elf.split('\n')) for elf in f.read().strip().split('\n\n')]
-calories.sort(reverse=True)
-print(sum(calories[:3]))
+calories = sorted(sum(int(line) for line in elf.split('\n')) for elf in f.read().strip().split('\n\n'))
+print(sum(calories[-3:]))
